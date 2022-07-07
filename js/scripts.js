cbe81b1ffbb9376f5652baf7c31479d7ff198eb0
Farkhod Sadykov <farkhod.sadykov@gmail.com>
2022-07-07 18:23:15 +0200

    CSS/HTML/JS minor fixes. Added few simple commands, changed banner, UI fixes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-const LOGO = `
-████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ██╗         ██████╗  █████╗  ██████╗ ███████╗
-╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗██║         ██╔══██╗██╔══██╗██╔════╝ ██╔════╝
-   ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║███████║██║         ██████╔╝███████║██║  ███╗█████╗  
-   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██╔══██║██║         ██╔═══╝ ██╔══██║██║   ██║██╔══╝  
-   ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██║  ██║███████╗    ██║     ██║  ██║╚██████╔╝███████╗
-   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝    ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝
+const LOGO = `>
+███████╗██╗  ██╗███████╗██╗     ██╗     
+██╔════╝██║  ██║██╔════╝██║     ██║     
+███████╗███████║█████╗  ██║     ██║     
+╚════██║██╔══██║██╔══╝  ██║     ██║     
+███████║██║  ██║███████╗███████╗███████╗
+╚══════╝╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
 `;
-
-const ABOUT = 'Hello world, I\'m a Software Engineer based in Czech Republic, Prague with 4+ years experience working on scalable web applications, web services and cloud infrastructure. I have experience in developing and deploying client - server API applications based on Django Rest Framework/Flask (Python), Express.js (NodeJS), RabbitMQ, SQL/NoSQL databases, Nginx, Docker, Google cloud/AWS. I’m a fan of design patterns and development methodologies like OOP, Unit testing and always try to contribute my knowledge to development process.';
-
-const PS1 = 'web@guest: ';
+const ABOUT = "Hello world, I'm a Software Engineer based in Czech Republic, Prague with 4+ years experience working on scalable web applications, web services and cloud infrastructure. I have experience in developing and deploying client - server API applications based on Django Rest Framework/Flask (Python), Express.js (NodeJS), RabbitMQ, SQL/NoSQL databases, Nginx, Docker, Google cloud/AWS. I’m a fan of design patterns and development methodologies like OOP, Unit testing and always try to contribute my knowledge to development process.";
+const github = 'https://github.com/TheR1D';
+const linkedIn = 'https://linkedin.com/in/farkhod-sadykov';
+const PS1 = 'web@guest:' + ' ';
 
 
 function appendElementToTerminal(childElement) {
@@ -24,10 +24,22 @@ function appendTextToTerminal(text, tag = 'p') {
   appendElementToTerminal(newElement);
 }
 
+function appendLinkToTerminal(link) {
+  const textNode = document.createTextNode(link);
+  const paragraphElement = document.createElement('p');
+  const hrefElement = document.createElement('a');
+  hrefElement.appendChild(textNode);
+  hrefElement.href = link;
+  hrefElement.title = link;
+  // parahraphElement.classList.add('terminal-text');
+  paragraphElement.appendChild(hrefElement);
+  appendElementToTerminal(paragraphElement);
+}
+
 function makeHookForInputEnter() {
-  const input = document.getElementById("terminal-input");
-  input.addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
+  const input = document.getElementById('terminal-input');
+  input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
       event.preventDefault();
       appendTextToTerminal(PS1 + input.value);
       if (input.value in commands) {
@@ -42,9 +54,8 @@ function makeHookForInputEnter() {
       terminalElement.scrollTop = terminalElement.scrollHeight;
     }
   });
-  input.addEventListener("blur", function(event) {
-    input.focus();
-  });
+  // Force input field to be in focus forever.
+  input.addEventListener('blur', (event) => input.focus());
 }
 
 function clearCommand() {
@@ -57,13 +68,18 @@ function aboutCommand() {
   appendTextToTerminal(ABOUT);
 }
 
+githubCommand = () => appendLinkToTerminal(github);
+linkedInCommand = () => appendLinkToTerminal(linkedIn);
+
 window.onload = () => {
   commands = {
     clear: clearCommand,
     about: aboutCommand,
+    github: githubCommand,
+    linkedin: linkedInCommand,
   }
   // Since we need list of all commands for help text.
-  const commandsList = Object.keys(commands).join(', ') + ', help';
+  const commandsList = Object.keys(commands).sort().join(', ') + ', help';
   const helpText = 'You can use terminal by typing commands. List of available commands: ' + commandsList; 
   commands.help = () => appendTextToTerminal(helpText);
 
